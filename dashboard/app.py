8a4ea7ceb8a2a01b27ce71df74bb7c0db3a7d0d2
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-08-04 22:08:30 -0400

    feat: two columns layout

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 from data import gapminder, migrantes
 from scatter import get_scatter, get_bubble, get_animated
 from heatmap import get_heatmap
@@ -11,20 +12,45 @@ if is_gunicorn:
 else:
     requests_pathname_prefix = "/"
 
-app = Dash(__name__, requests_pathname_prefix=requests_pathname_prefix)
+app = Dash(
+    __name__,
+    requests_pathname_prefix=requests_pathname_prefix,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+)
 server = app.server
 
-app.layout = html.Div(
+app.layout = dbc.Container(
     children=[
-        html.H1("Ejercicio 1 de la tarea 2"),
-        dcc.Graph(id="scatter", figure=get_scatter(gapminder, 2007)),
-        html.H1("Ejercicio 2 de la tarea 2"),
-        dcc.Graph(id="bubble", figure=get_bubble(gapminder, 2007)),
-        html.H1("Ejercicio 3 de la tarea 2"),
-        dcc.Graph(id="animated", figure=get_animated(gapminder)),
-        html.H1("Ejercicio 5 de la tarea 2"),
-        dcc.Graph(id="heatmap", figure=get_heatmap(migrantes)),
-    ]
+        dbc.Row(
+            [
+                dbc.Col(html.H1("Ejercicio 1 de la tarea 2"), width=3),
+                dbc.Col(dcc.Graph(id="scatter", figure=get_scatter(gapminder, 2007))),
+            ],
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.H1("Ejercicio 2 de la tarea 2"), width=3),
+                dbc.Col(dcc.Graph(id="bubble", figure=get_bubble(gapminder, 2007))),
+            ],
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.H1("Ejercicio 3 de la tarea 2"), width=3),
+                dbc.Col(dcc.Graph(id="animated", figure=get_animated(gapminder))),
+            ],
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.H1("Ejercicio 5 de la tarea 2"), width=3),
+                dbc.Col(dcc.Graph(id="heatmap", figure=get_heatmap(migrantes))),
+            ],
+            align="center",
+        ),
+    ],
+    className="p-5",
 )
 
 
